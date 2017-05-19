903882695e434e5099ff497350611abafc6619dc
Theodosis Melidis <melidisteo@Gmail.com>
2017-05-19 22:19:51 +0300

    Update hashTable.h

--- source/hashTable.h
+++ source/hashTable.h
@@ -8,16 +8,16 @@ class hashTable{
 	private:
 		struct neighborsOfV{
 			long v;
-			avlTree neighbors;      //Ένα AVL δέντρο με τους γείτονες του στοιχείου
+			avlTree neighbors;      //an AVL tree with the neighbors of the element
 		}typedef neighborsOfV;
 
-		long D; 				// Για |V| = 44953 κορυφές, ο πρώτος πρώτος αριθμός μεγαλύτερος
-								// του 2|V| ώστε να επιτευχθεί τουλάχιστον παράγοντας φόρτωσης 0.5
-		neighborsOfV* ht;   	//o HashTable
-		bool *empty;    		//Δείχνει αν μια θέση του HashTable είναι κενή ή όχι
-		bool *isInSetS;         //Γίνεται αληθής όταν το στοιχείο του bucket έχει μπει στο σύνολο S
-		long vectorsInS; 		//Το πλήθος των κορυφών που μπαίνουν στο S
-		bool* neverUsed;		//Σημαία που χρησιμοποιείται για τη διαγραφή στοιχείων του πίνακα
+		long D; 			// For |V| = 44953 vertices, the first prime numnber that is bigger
+						// than 2|V| such as to be achieved loading factor at least 0.5
+		neighborsOfV* ht;   		//hashTable
+		bool *empty;    		//it shows if a position of hashTable is empty or not
+		bool *isInSetS;         	//it becomes true if bucket's element has entered set S
+		long vectorsInS; 		//the number of vertices that entered S
+		bool* neverUsed;		//a flag that is used for the deletion of array's elements
 
 
 	public:
